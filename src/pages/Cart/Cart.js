2034229e91fa0b9b8207b5a1ec79196c58cb3122
davariijs <Atuor127@gmail.com>
2024-12-12 01:22:08 -0500

    update Cart

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -1,7 +1,7 @@
 import React , { Fragment } from "react";
 import { Link } from "react-router-dom";
 import leftArrowIcon from "../../assets/icons/left-arrow.svg";
-
+import "./Cart.css";
 
 export default function Cart() {
 
@@ -10,6 +10,7 @@ export default function Cart() {
         <Fragment>
                 <div className="relative">
                     <div className="absolute bg-darkText w-full h-20 top-48 z-0"></div>
+                    <div className="absolute bg-secondary z-0 w-full bottom-0 checkout-bg"></div>
                 <div className="container mx-auto px-5 md:px-0">
                     
                     <div className="pt-14 pb-6 flex lg:text-lg"><Link to="/" className="text-grayText font-normal  pr-3">Home</Link><img src={leftArrowIcon} width="5px" height="10.14px" alt="arrow"/>
@@ -32,12 +33,45 @@ export default function Cart() {
                                 </div>
                             </div>
 
-                            <div className="h-7 w-full">
+                            <div className=" w-full">
 
                             </div>
 
                         </div>
-                        <div></div>
+                        <div className="lg:flex justify-between mt-10">
+                            <div className="flex flex-col justify-center items-center lg:items-start">
+                                <h4 className="text-darkText font-semibold text-2xl">Discount  Codes</h4>
+                                <h5 className="text-grayText font-normal text-base mt-2">Enter your coupon code if you have one</h5>
+                                <div className="my-10">
+                                <input className="border-2 border-borderGrey bg-white py-3 px-5 rounded-l-xl"/>
+                                <button className="bg-primary font-semibold text-white py-3 px-5 text-base rounded-r-xl border-y-2 border-r-2 border-borderGrey">Apply Coupon</button>
+                                </div>
+                                <button className="bg-white text-base text-darkText font-semibold py-3 px-5 rounded-lg border-2 border-borderGrey w-fit">Continue Shopping</button>
+                            </div>
+
+                            <div className="bg-grayCart py-10 lg:mt-0 mt-12">
+                                <div className="flex justify-between px-20  text-darkText font-medium text-xl">
+                                    <span>Sub Total </span> 
+                                    <span>$513.00</span>
+                                </div>
+
+                                <div className="flex justify-between pt-3 px-20 text-darkText font-medium text-xl">
+                                    <span>Shipping</span> 
+                                    <span>$5.00</span>
+                                </div>
+
+                                <div className="flex justify-between py-9 px-20 text-darkText font-bold text-xl">
+                                    <span>Grand Total</span> 
+                                    <span>$518.00</span>
+                                </div>
+                                
+                                <hr className="text-borderGrey"/>
+
+                                <div className="flex justify-center mt-10">
+                                <button className="bg-primary font-semibold text-white text-lg py-3 px-5 rounded-lg">Proceed To Checkout</button>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 </div>
