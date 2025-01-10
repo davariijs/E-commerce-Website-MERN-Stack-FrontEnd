494a54aabb470c39c7529a3e9c49e517fea2592c
davariijs <atuor127@gmail.com>
2025-01-10 23:50:21 +0330

    add firebase to reset password

--- src/pages/Login-SignUp/ResetPassword.js
+++ src/pages/Login-SignUp/ResetPassword.js
@@ -1,9 +1,33 @@
-import React, { Fragment } from "react";
-import { Link } from "react-router-dom";
+import React, { Fragment, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import resetImage from "../../assets/images/reset-password.webp"
 import "./login-signup.css";
+import { auth} from "../../firebase/firebase"; 
+import { sendPasswordResetEmail } from "firebase/auth";
 
 export default function ResetPassword() {
+
+    const [email, setEmail] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
+    const navigate = useNavigate(); 
+
+    const resetPassword = (e) => {
+        e.preventDefault();
+        sendPasswordResetEmail(auth, email)
+        .then(() => {
+            // Password reset email sent!
+            // ..
+            setEmail("");
+            setTimeout(navigate("/"), 3000);
+            console.log("ok");
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            setErrorMessage(errorMessage)
+            // ..
+        });
+    }
 
     return(
         <Fragment>
@@ -19,16 +43,16 @@ export default function ResetPassword() {
 
                     
 
-                    <form>
+                    <form onSubmit={resetPassword}>
                     <div className="mt-11">
                     <label className="font-medium text-lg text-darkText ">Email</label>
-                    <input className="rounded-lg border-2 w-full border-borderLight border-opacity-80 py-4 px-4 mt-2 mb-2" type="text" name="" id="" placeholder="example@gmail.com"/>
-                    <span className="font-medium text-red text-base"><h5>We can not find your email.</h5></span>
+                    <input onChange={(e)=> setEmail(e.target.value)} value={email} className="rounded-lg border-2 w-full border-borderLight border-opacity-80 py-4 px-4 mt-2 mb-2" type="text" name="" id="" placeholder="example@gmail.com"/>
+                    <span className="font-medium text-red text-base"><h5>{errorMessage}</h5></span>
                     </div>
 
                     <div className="mt-11">
                         <button className="bg-primary rounded-lg text-white px-5 py-4 text-center items-center md:w-40 w-full mb-2">Send</button>
-                        <h4 className="font-medium text-base text-grayText">Back to <Link className="underline">Login</Link></h4>
+                        <h4 className="font-medium text-base text-grayText">Back to <Link to="/login" className="underline">Login</Link></h4>
                     </div>
                     </form>
                     
