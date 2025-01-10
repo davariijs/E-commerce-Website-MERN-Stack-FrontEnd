e1b3fdc6a1e97956e00ace86a5691a0f32f45b19
davariijs <atuor127@gmail.com>
2025-01-11 00:41:12 +0330

    fix check email

--- src/pages/Login-SignUp/CheckEmail.js
+++ src/pages/Login-SignUp/CheckEmail.js
@@ -17,13 +17,13 @@ export default function CheckEmail() {
                     <div className="">
                     <h2 className="font-bold text-3xl text-darkText md:text-left  text-center ">Check Email</h2>
                     <h3 className="mt-8 ">Please check your email inbox and click on the provided link to reset your
-                    password.<br/> If you don’t receive email, <button className="text-primary font-bold">Click here to resend</button></h3>
+                    password.<br/> If you don’t receive email, <Link to="/reset-password" className="text-primary font-bold">Click here to resend</Link></h3>
 
                     
 
                     <div className="mt-8 flex items-center">
                     <IoIosArrowBack className="text-grayText mr-2"/>
-                    <h4 className="font-medium text-base text-grayText">Back to <Link className="underline">Login</Link></h4>
+                    <h4 className="font-medium text-base text-grayText">Back to <Link to="/login" className="underline">Login</Link></h4>
                     </div>
                     </div>
                     
