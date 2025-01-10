5d195f097c85be5c6a72675b2b42b358a3609d0b
davariijs <atuor127@gmail.com>
2025-01-11 00:40:53 +0330

    fix reset password

--- src/pages/Login-SignUp/ResetPassword.js
+++ src/pages/Login-SignUp/ResetPassword.js
@@ -18,8 +18,8 @@ export default function ResetPassword() {
             // Password reset email sent!
             // ..
             setEmail("");
-            setTimeout(navigate("/"), 3000);
-            console.log("ok");
+            setErrorMessage("")
+            setTimeout(navigate("/check-email"), 3000);
         })
         .catch((error) => {
             const errorCode = error.code;
@@ -31,15 +31,15 @@ export default function ResetPassword() {
 
     return(
         <Fragment>
-            <div className="md:grid md:grid-cols-2 ">
+            <div className="md:grid md:grid-cols-2 flex flex-col-reverse ">
                 <div className="">
-                    <img src={resetImage} className="h-screen w-full" alt="clothes"/>
+                    <img src={resetImage} className="h-screen w-full  md:relative  absolute top-0 -z-10" alt="clothes"/>
                 </div>
 
-                <div className="mx-14 pt-10 md:mx-auto md:px-4">
+                <div className="mx-0 pt-10 md:pb-0 pb-10 md:mx-auto md:px-6 px-6 bg-white bg-opacity-75">
                     <div className="">
                     <h2 className="font-bold text-3xl text-darkText md:text-left  text-center ">Reset Your Password</h2>
-                    <h3>Enter your email and we'll send you a link to reset your password.<br/>Please  check it.</h3>
+                    <h3 className="md:text-start text-center">Enter your email and we'll send you a link to reset your password.</h3>
 
                     
 
@@ -52,7 +52,7 @@ export default function ResetPassword() {
 
                     <div className="mt-11">
                         <button className="bg-primary rounded-lg text-white px-5 py-4 text-center items-center md:w-40 w-full mb-2">Send</button>
-                        <h4 className="font-medium text-base text-grayText">Back to <Link to="/login" className="underline">Login</Link></h4>
+                        <h4 className="font-medium text-base text-grayText md:text-start text-center">Back to <Link to="/login" className="underline">Login</Link></h4>
                     </div>
                     </form>
                     
