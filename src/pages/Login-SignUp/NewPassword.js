263cbdce4890434b96569c8b9421f4feb139a14b
davariijs <atuor127@gmail.com>
2025-01-11 00:41:00 +0330

    fix new password

--- src/pages/Login-SignUp/NewPassword.js
+++ src/pages/Login-SignUp/NewPassword.js
@@ -1,34 +1,63 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
+import {useNavigate } from "react-router-dom";
 import newPasswordImg from "../../assets/images/new-password.png";
 import "./login-signup.css";
+import { auth} from "../../firebase/firebase"; 
+import { updatePassword  } from "firebase/auth";
 
 export default function NewPassword() {
 
+    const [newPassword, setNewPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
+    const navigate = useNavigate();
+    const user = auth.currentUser; 
+
+    const newPasswordFunc = (e) => {
+        e.preventDefault();
+        if (newPassword === confirmPassword){
+        updatePassword(user, newPassword).then(() => {
+            // Update successful.
+            console.log("Update successful.");
+            setErrorMessage("")
+            setTimeout(navigate("/login"), 3000);
+          }).catch((error) => {
+            // An error ocurred
+            // ...
+            console.log(error);
+            const errorMessagePassword = error.message;
+            setErrorMessage(errorMessagePassword)
+          });
+        } else {
+            setErrorMessage("New password and confirm new password do not match")
+        }
+    }
+
     return(
         <Fragment>
-            <div className="md:grid md:grid-cols-2 ">
+            <div className="md:grid md:grid-cols-2 flex flex-col-reverse ">
                 <div className="">
-                    <img src={newPasswordImg} className="h-screen w-full" alt="clothes"/>
+                    <img src={newPasswordImg} className="h-screen w-full   md:relative  absolute top-0 -z-10" alt="clothes"/>
                 </div>
 
-                <div className="mx-14 pt-10 md:mx-auto md:px-4">
+                <div className="mx-0 pt-10 md:pb-0 pb-10 md:mx-auto md:px-4 px-6 bg-white bg-opacity-75">
                     <div className="">
                     <h2 className="font-bold text-3xl text-darkText md:text-left  text-center ">Create New Password</h2>
                     <h3 className="text-grayText">Your new password must be different from previous used passwords.</h3>
 
                     
 
-                    <form>
+                    <form onSubmit={newPasswordFunc}>
                     <div className="mt-11">
                     <label className="font-medium text-lg text-darkText ">Password</label>
-                    <input className="rounded-lg border-2 w-full border-borderLight border-opacity-80 py-4 px-4 mt-2 mb-2" type="text"/>
+                    <input onChange={(e)=> setNewPassword(e.target.value)} value={newPassword} className="rounded-lg border-2 w-full border-borderLight border-opacity-80 py-4 px-4 mt-2 mb-2" type="text"/>
                     <span className="font-medium text-grayText text-base"><h5>Must be at least 8 characters.</h5></span>
                     </div>
 
                     <div className="mt-11">
                     <label className="font-medium text-lg text-darkText ">Confirm Password</label>
-                    <input className="rounded-lg border-2 w-full border-borderLight border-opacity-80 py-4 px-4 mt-2 mb-2" type="text"/>
-                    <span className="font-medium text-red text-base"><h5>New password and confirm new password do not match</h5></span>
+                    <input onChange={(e)=> setConfirmPassword(e.target.value)} value={confirmPassword} className="rounded-lg border-2 w-full border-borderLight border-opacity-80 py-4 px-4 mt-2 mb-2" type="text"/>
+                    <span className="font-medium text-red text-base"><h5>{errorMessage}</h5></span>
                     </div>
 
                     <div className="mt-11">
