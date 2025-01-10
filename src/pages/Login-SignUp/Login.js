510183c16f586a6b0511139bb8e733206a92642f
davariijs <Atuor127@gmail.com>
2025-01-09 22:30:59 -0500

    Add login methods

--- src/pages/Login-SignUp/Login.js
+++ src/pages/Login-SignUp/Login.js
@@ -1,26 +1,91 @@
-import React, { Fragment } from "react";
-import { IoMdEyeOff } from "react-icons/io";
-import { IoMdEye } from "react-icons/io";
-import { Link } from "react-router-dom";
+import React, { Fragment, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { FcGoogle } from "react-icons/fc";
-import { FcPhone } from "react-icons/fc";
-import loginImage from "../../assets/images/login-image.webp"
+import loginImage from "../../assets/images/login-image.webp";
+import {Icon} from 'react-icons-kit';
+import {eyeOff} from 'react-icons-kit/feather/eyeOff';
+import {eye} from 'react-icons-kit/feather/eye';
+import { auth,googleProvider } from "../../firebase/firebase"; 
+import { signInWithPopup, signInWithEmailAndPassword, GoogleAuthProvider } from "firebase/auth";
 import "./login-signup.css";
+
 
 export default function Login() {
 
+    const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
+    const [type, setType] = useState('password');
+    const [icon, setIcon] = useState(eyeOff);
+    const navigate = useNavigate(); 
+
+    const handleToggle = () => {
+        if (type==='password'){
+           setIcon(eye);
+           setType('text')
+        } else {
+           setIcon(eyeOff)
+           setType('password')
+        }
+     }
+
+
+         const handleLoginGoogle = async (e) => {
+            signInWithPopup(auth, googleProvider)
+                .then((result) => {
+                    // This gives you a Google Access Token. You can use it to access the Google API.
+                    const credential = GoogleAuthProvider.credentialFromResult(result);
+                    const token = credential.accessToken;
+                    // The signed-in user info.
+                    const user = result.user;
+                    // IdP data available using getAdditionalUserInfo(result)
+                    console.log(user);
+                    setTimeout(navigate("/account"), 3000);
+                    // ...
+                }).catch((error) => {
+                    // Handle Errors here.
+                    const errorCode = error.code;
+                    const errorMessage = error.message;
+                    // The email of the user's account used.
+                    const email = error.customData.email;
+                    // The AuthCredential type that was used.
+                    const credential = GoogleAuthProvider.credentialFromError(error);
+                    console.log(errorMessage);
+                    // ...
+                });
+            } 
+
+
+                const signinEmail = (e) => {
+                    e.preventDefault();
+                    signInWithEmailAndPassword(auth, email, password)
+                    .then((userCredential) => {
+                        // Signed in 
+                        const user = userCredential.user;
+                        setTimeout(navigate("/account"), 3000);
+                        // ...
+                    })
+                    .catch((error) => {
+                        const errorCode = error.code;
+                        const errorMessage = error.message;
+                        setErrorMessage(errorMessage)
+                        console.log(errorMessage);
+                    });
+                }
+
+
     return(
         <Fragment>
-            <div className="md:grid md:grid-cols-2 ">
+            <div className="relative">
+            <div className="md:grid md:grid-cols-2 flex flex-col-reverse ">
                 <div className="">
-                    <img src={loginImage} className="h-screen w-full" alt="clothes"/>
+                    <img src={loginImage} className="h-screen w-full md:relative  absolute top-0 -z-10" alt="clothes"/>
                 </div>
 
-                <div className="mx-14 pt-10 md:mx-auto md:px-4">
+                <div className="mx-0 pt-10 md:pb-0 pb-10 md:mx-auto md:px-0 px-6 bg-white bg-opacity-75">
                     <div className="">
                     <h2 className="font-bold text-3xl text-darkText md:text-left  text-center ">LogIn</h2>
-                    <button className="rounded-lg border-2 w-full text-primary font-medium text-xl border-darkText flex justify-center items-center py-4 px-5 mt-11 mb-5"><FcGoogle className="w-5 h-5 mr-2"/>Continue With Google</button>
-                    <button className="rounded-lg border-2 w-full text-primary font-medium text-xl border-darkText flex justify-center items-center py-4 px-5 mb-11"><FcPhone className="w-5 h-5 mr-2"/>Continue With Number</button>
+                    <button onClick={handleLoginGoogle} className="rounded-lg border-2 w-full text-primary font-medium text-xl border-darkText flex justify-center items-center py-4 px-5 mt-11 mb-5"><FcGoogle className="w-5 h-5 mr-2"/>Continue With Google</button>
 
                     <div className="flex text-grayText justify-center text-center items-center">
                         <div className="w-full divPart rounded-lg bg-grayText mr-4"></div>
@@ -28,27 +93,29 @@ export default function Login() {
                         <div className="w-full divPart rounded-lg bg-grayText ml-4"></div>
                     </div>
 
-                    <form>
+                    <form onSubmit={signinEmail}>
                     <div className="mt-8">
-                    <label className="font-medium text-lg text-darkText ">Email address or Phone number </label>
-                    <input className="rounded-lg border-2 w-full border-darkText py-4 px-4 mt-2" type="text" name="" id=""/>
+                    <label className="font-medium text-lg text-darkText ">Email address</label>
+                    <input onChange={(e)=> setEmail(e.target.value)} value={email} className="rounded-lg border-2 w-full border-darkText py-4 px-4 mt-2" type="text" name="" id=""/>
+                    <span className="font-medium text-red text-sm"><h5>{errorMessage}</h5></span>
                     </div>
 
                     <div>
-                    <div className="font-medium text-lg text-darkText flex justify-between items-center mt-8"><label>Password</label> <button className="flex justify-center items-center text-grayText"><IoMdEye className="mr-2"/><IoMdEyeOff className="mr-2"/>Hide</button></div>
-                    <input className="rounded-lg border-2 w-full border-darkText py-4 px-4 mt-2 mb-2"  type="text" name="" id=""/>
-                    <Link className="font-medium text-base text-darkText text-right underline"><h5>Forget your password</h5></Link>
+                    <div className="font-medium text-lg text-darkText flex justify-between items-center mt-8"><label>Password</label> <span onClick={handleToggle} className="flex justify-center items-center text-grayText"><Icon class="absolute mr-8" icon={icon} size={20}/></span></div>
+                    <input autoComplete="current-password" onChange={(e)=> setPassword(e.target.value)} value={password} className="rounded-lg border-2 w-full border-darkText py-4 px-4 mt-2 mb-2"  type={type} name="" id=""/>
+                    <Link to="/reset-password" className="font-medium text-base text-darkText text-right underline"><h5>Forget your password</h5></Link>
                     </div>
 
                     <div className="mt-11">
                         <button className="bg-primary rounded-lg text-white px-5 py-4 text-center items-center md:w-40 w-full mb-2">Sign In</button>
-                        <h4 className="font-medium text-base text-darkText">Don’t have an account?<Link className="underline">Sign up</Link></h4>
+                        <h4 className="font-medium text-base text-darkText">Don’t have an account?<Link to="/sign-up" className="underline">Sign up</Link></h4>
                     </div>
                     </form>
                     
                     </div>
                     
                 </div>
+            </div>
             </div>
         </Fragment>
     )
