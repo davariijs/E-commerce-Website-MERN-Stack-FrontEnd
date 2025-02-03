65c379d89731ffc8976f715d16cfe7635323c533
davariijs <Atuor127@gmail.com>
2025-02-02 21:08:26 -0500

    Fix components

---
+++ src/components/Loading/loading.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import loadingBar from "../../assets/images/loader.svg";
+const Loading: React.FC = () => {
+  return <div className='flex justify-center items-center h-fit w-full relative'><img className='w-36' src={loadingBar} alt='loading ...'/></div>;
+};
+
+export default Loading;
