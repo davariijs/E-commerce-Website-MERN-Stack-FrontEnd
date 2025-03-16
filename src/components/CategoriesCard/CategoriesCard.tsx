0667517ab8c9ddacf1ca7a89adc8205ceea71f6b
davariijs <Atuor127@gmail.com>
2025-03-15 22:05:03 -0400

    enhance CategoriesCard with AOS integration and hover effects

--- src/components/CategoriesCard/CategoriesCard.tsx
+++ src/components/CategoriesCard/CategoriesCard.tsx
@@ -1,6 +1,8 @@
-import React, { Fragment} from "react";
+import React, { Fragment, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { GoArrowRight } from "react-icons/go";
+import AOS from "aos";
+import "aos/dist/aos.css";
 import "./CategoriesCard.css";
 import { Img } from "react-image";
 
@@ -16,12 +18,13 @@ type TCategoriesCard ={
 
 export default function CategoriesCard({srcCategoriesCard, textCategoriesCard,price,categoriesFashionCard,brand,linkCard,onClick}:TCategoriesCard) {
 
-
-    
+    useEffect(() => {
+        AOS.init({ duration: 1000, once: false });
+    }, []);
 
     return (
         <Fragment>
-            <div className="categoriesCardSection">
+            <div className="categoriesCardSection" data-aos="fade-up">
             
             <div className="relative categoriesCardImgPart">
             <Link to={String(linkCard)}>
@@ -37,7 +40,7 @@ export default function CategoriesCard({srcCategoriesCard, textCategoriesCard,pr
             </Link>
                 {categoriesFashionCard ? <div></div> : <div onClick={onClick} className="cardSvgLike absolute bg-white w-8 h-8 flex justify-center lg:top-6 lg:right-7 top-2 right-2 z-10 cursor-pointer" style={{borderRadius:"50%"}}>
                     <svg className="likeSvg" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path fill-rule="evenodd" clip-rule="evenodd" d="M9.99486 4.93014C8.49535 3.18262 5.99481 2.71255 4.11602 4.31275C2.23723 5.91295 1.97273 8.5884 3.44815 10.481C4.67486 12.0545 8.38733 15.3732 9.60407 16.4474C9.7402 16.5675 9.80827 16.6276 9.88766 16.6512C9.95695 16.6718 10.0328 16.6718 10.1021 16.6512C10.1815 16.6276 10.2495 16.5675 10.3857 16.4474C11.6024 15.3732 15.3149 12.0545 16.5416 10.481C18.017 8.5884 17.7848 5.89611 15.8737 4.31275C13.9626 2.72938 11.4944 3.18262 9.99486 4.93014Z" stroke="" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path fillRule="evenodd" clipRule="evenodd" d="M9.99486 4.93014C8.49535 3.18262 5.99481 2.71255 4.11602 4.31275C2.23723 5.91295 1.97273 8.5884 3.44815 10.481C4.67486 12.0545 8.38733 15.3732 9.60407 16.4474C9.7402 16.5675 9.80827 16.6276 9.88766 16.6512C9.95695 16.6718 10.0328 16.6718 10.1021 16.6512C10.1815 16.6276 10.2495 16.5675 10.3857 16.4474C11.6024 15.3732 15.3149 12.0545 16.5416 10.481C18.017 8.5884 17.7848 5.89611 15.8737 4.31275C13.9626 2.72938 11.4944 3.18262 9.99486 4.93014Z" stroke="" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
                     </svg>
                 </div>}
             </div>
@@ -51,7 +54,11 @@ export default function CategoriesCard({srcCategoriesCard, textCategoriesCard,pr
                 </div>
 
                 <div>
-                    {categoriesFashionCard ?  <div className="text-darkText mt-5"><GoArrowRight /></div> : <div className="text-darkText bg-secondary lg:px-4 lg:py-2 md:px-4 md:py-2 px-2 py-1 font-bold text-sm rounded-lg mt-4">${price}</div>}
+                    {categoriesFashionCard ?  
+                        <div className="text-darkText mt-5"><GoArrowRight /></div> 
+                        : 
+                        <div className="text-darkText bg-secondary lg:px-4 lg:py-2 md:px-4 md:py-2 px-2 py-1 font-bold text-sm rounded-lg mt-4">${price}</div>
+                    }
                 </div>
                 </div>
                 
